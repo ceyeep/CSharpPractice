6c38e1b64ef2768c28aca578a286be8d24e94d3e
ceyeep <ceyeep@gmail.com>
2015-02-05 01:00:27 -0700

    Adding unit tets

---
+++ PracticeExercises/UnitTests/SortAlgorithmsTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticeExercises;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SortAlgorithmsTests
+    {
+        List<int> originalList = new List<int>(new int[] { 5, 4, 3, 1, 2 });
+        List<int> sortedList = new List<int>(new int[] { 1, 2, 3, 4, 5 });
+
+        [TestMethod]
+        public void TestBubbleSort()
+        {
+            SortAlgorithms.bubleSort(originalList);
+            CollectionAssert.Equals(sortedList, originalList);
+
+        }
+    }
+}
